3a07736fbdd0783813b33d063d84f6dfe1cfa948
Jonathan Puckey <me@jonathanpuckey.com>
2013-03-28 20:41:28 +0100

    When no width argument is passed, pass back the unchanged number as a string.

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ function pad(number, width, padding) {
   padding = padding || '0';
   number = number + '';
   var length = number.length;
-  return length >= width
-  		? number
-  		: new Array(width - length + 1).join(padding) + number;
+  return !width || length >= width
+		? number + ''
+		: new Array(width - length + 1).join(padding) + number;
 }
 
 module.exports = pad;
