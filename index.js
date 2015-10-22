49f1c3354e2cc04cda35e42a5a914201e35e24ee
Ronald Loyko <ronald.loyko@gmail.com>
2015-10-22 10:19:32 +0200

    Added comments and did some refactoring.
    - Added documentation comments.
    - Added function comments.
    - Added banner.
    - Function throws now an Error if no number has been provided as argument.

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
+'use strict';
+
+/**
+ * @description Convert a number to a string with
+ * a given amount of leading characters.
+ * @param {number} number Number to convert.
+ * @param {number} width Amount of leading characters to prepend.
+ * @param {string} [padding = '0'] leading character.
+ * @throws Error
+ * @returns {string}
+ */
 function pad(number, width, padding) {
+
+  // Throw exception if no number has been provided.
+  if (!number) throw new Error('Missing first argument');
+
+  // Set padding character.
   padding = padding || '0';
+
+  // Convert number to string.
   number = number + '';
-  var length = number.length;
-  return !width || length >= width
-		? '' + number
-		: new Array(width - length + 1).join(padding) + number;
+
+  // Return either the original number as string,
+  // or the number with leading padding characters.
+  if (!width || number.length >= width) return number;
+  else return new Array(width - number.length + 1).join(padding) + number;
 }
 
 module.exports = pad;
